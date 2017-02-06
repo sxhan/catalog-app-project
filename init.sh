0fb12b31cd6297f9cb43d3543c8b361a17dd9d1e
Simon X Han <han.simonx@gmail.com>
2017-02-05 21:28:39 -0800

    Add models

---
+++ init.sh
@@ -0,0 +1,1 @@
+python item_catalog/models.py
---
+++ item_catalog/models.py
@@ -0,0 +1,97 @@
+import datetime as dt
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    course = Column(String(250))
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    restaurant = relationship(Restaurant)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'course': self.course,
+        }
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    deleted = Column(Boolean)
+    created_date = Column(DateTime,
+                          default=dt.datetime.utcnow())
+    updated_date = Column(DateTime,
+                          default=dt.datetime.utcnow(),
+                          onupdate=dt.datetime.utcnow())
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Item(Base):
+    __tablename__ = "item"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    description = Column(String(250))
+    deleted = Column(Boolean)
+    created_date = Column(DateTime,
+                          default=dt.datetime.utcnow())
+    updated_date = Column(DateTime,
+                          default=dt.datetime.utcnow(),
+                          onupdate=dt.datetime.utcnow())
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'category_id': self.category_id
+        }
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+
+Base.metadata.create_all(engine)
