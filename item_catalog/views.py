d70e5c33cda53024a07823a9409ffea916e1ef05
Simon X Han <han.simonx@gmail.com>
2017-02-09 23:11:14 -0800

    Logout

--- item_catalog/views.py
+++ item_catalog/views.py
@@ -524,24 +524,10 @@ def fbdisconnect():
 # Add generalized disconnect function based on provider
 @app.route('/disconnect')
 def disconnect():
-    if 'provider' in login_session:  # To do this, we must add this in each of the login functions!
-        if login_session['provider'] == 'google':
-            gdisconnect()
-            del login_session['gplus_id']
-            del login_session['credentials']
-        if login_session['provider'] == 'facebook':
-            fbdisconnect()
-            del login_session['facebook_id']
-        del login_session['username']
-        del login_session['email']
-        del login_session['picture']
-        del login_session['user_id']
-        del login_session['provider']
-        flash("You have successfully been logged out.")
-        return redirect(url_for('showRestaurants'))
-    else:
-        flash("You were not logged in")
-        return redirect(url_for('showRestaurants'))
+    if "session_info" in session:
+        del session["session_info"]
+    return Logout()
+
 
 # Simple HTTP error handling
 @app.errorhandler(404)
